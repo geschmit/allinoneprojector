34231fe20b243d04117906b8620e993d8e99eac0
Gabe Schmit <119266089+geschmit@users.noreply.github.com>
2023-08-31 14:08:54 +0000

    fixes

--- main.ts
+++ main.ts
@@ -6,11 +6,23 @@ const app = new Hono()
 app.use("*",logger())
 app.use("/", serveStatic({ path: "asset/index.html" }))
 app.use("/admin.html", serveStatic({ path: "asset/admin.html" }))
+app.use("/video.mp4", serveStatic({ path: "asset/video.mp4" }))
 
-app.post('/api/upload', async (c) => {
+let connectedSockets:Array<Deno.WebSocketUpgrade> = []
+app.get("/api/websocket", async (c) => {
+    const sock = Deno.upgradeWebSocket(c.req.raw)
+    connectedSockets.push(sock)
+    return sock.response
+})
+
+app.post("/api/upload", async (c) => {
     const data = await c.req.formData()
-    Deno.writeFileSync("asset/video",data.get("file") as any)
-    return c.text("a")
+    Deno.writeFileSync("asset/video.mp4",data.get("file") as any)
+    for (const x of connectedSockets) {
+        x.socket.send("reload")
+    }
+    connectedSockets = []
+    return c.redirect("/")
 })
 
 Deno.serve(app.fetch)
